da4475029b2ea29794143b8648c7d2ab4c667cc4
DONGHAEY <dongheaey@gmail.com>
2024-05-29 20:28:38 +0900

    ADD :: file controller upload method

---
+++ src/file/file.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import JwtAuthGuard from 'src/auth/guards/auth.guard';
+import { FileService } from './file.service';
+
+@Controller('file')
+export class FileController {
+  constructor(private fileService: FileService) {}
+
+  @Post('/upload')
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(
+    @UploadedFile('file') file: Express.Multer.File,
+    @Body('path') path: string,
+  ) {
+    return await this.fileService.saveFileToSupabase(path, file.buffer);
+  }
+}
