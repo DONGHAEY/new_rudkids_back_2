9e136c83c5a95b3421b9cbce868d661d238045a4
DONGHAEY <dongheaey@gmail.com>
2024-07-03 16:21:56 +0900

    update

--- src/instagram-profile/instagram-profile.service.ts
+++ src/instagram-profile/instagram-profile.service.ts
@@ -29,7 +29,7 @@ export class InstagramProfileService {
       const imageFileResponse = await axios.get(instagramImgUrl, {
         responseType: 'arraybuffer',
       });
-      const fileName = `${instagramId}-instagram.png`;
+      const fileName = `${instagramId}-${new Date().toLocaleString()}-instagram.png`;
       const contentType =
         imageFileResponse?.headers?.['Content-Type']?.toString() ?? 'image/png';
       const savedInstagramImageUrl = await this.fileService.saveFileToSupabase(
