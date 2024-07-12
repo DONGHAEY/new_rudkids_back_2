4446f94d1e2baca7b9917c80bca9c629ffbfdbd6
DONGHAEY <dongheaey@gmail.com>
2024-07-13 03:57:00 +0900

    update :: invitation InvitedUserDto

--- src/invitation/dto/response/invited-user.dto.ts
+++ src/invitation/dto/response/invited-user.dto.ts
@@ -1,12 +1,12 @@
 import { Exclude, Expose } from 'class-transformer';
+import { InstagramEmbeded } from 'src/user/entity/embeded/instagram.embeded';
 import { UserEntity } from 'src/user/entity/user.entity';
 
 @Exclude()
 export class InvitedUserDto extends UserEntity {
   @Expose()
-  instagramId: string;
-  @Expose()
   nickname: string;
+
   @Expose()
-  imageUrl: string;
+  instagram: InstagramEmbeded;
 }
